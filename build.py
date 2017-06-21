ba688ba09d7db96ce9cd207c43c43c2bcc7d0670
nixen420 <nikhil.singh4008@gmail.com>
2017-06-21 17:19:48 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,12 @@
 import operator
-
+dic={1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
-
+    sorted_x = sorted(dic.items())
+    return sorted_x
+solution_asc(dic)
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
-
+    return sorted(dic.items(),key=operator.itemgetter(0),reverse=True)
+    return sorted_x
+solution_desc(dic)
